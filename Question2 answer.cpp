29a5cb896c2b75e2da98a4a99af0704a9a188b87
Sanjay Kumar <94333583+sanjay-72@users.noreply.github.com>
2023-01-13 18:18:37 +0530

    Update Question2 answer.cpp

--- Question2 answer.cpp
+++ Question2 answer.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<string.h>
+#include<string>
 
+using namespace std;
+class programming
+{
+	public:
+	programming()
+		{ 
+		cout<<"I love programming languages"<<endl ;
+		}
+	programming(char str[])
+		{
+			cout<<"I love "<<str<<endl;
+		}
+	};
+
+int main()
+{
+ programming o1;
+ char s[100];
+ cin>>s;
+programming o2(s);
+}
