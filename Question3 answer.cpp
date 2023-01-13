c5aacc6f1ce1060c7725f8f06158199772f1f718
Sanjay Kumar <94333583+sanjay-72@users.noreply.github.com>
2023-01-13 18:19:13 +0530

    Update Question3 answer.cpp

--- Question3 answer.cpp
+++ Question3 answer.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+class Area
+{
+public:
+    void output(int l, int b)
+    {
+        cout<<"Area of Rectangle = "<<l*b<<endl;
+    }
+void output(int a)
+    {
+        cout<<"Area of Square = "<<a*a<< endl;
+    }
+};
+int main()
+{
+    Area obj;
+    obj.output(3,3);
+    obj.output(3);
+}
