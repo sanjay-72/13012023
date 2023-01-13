b6fcc2586d0df57fd0cbb89549314ff2da80489b
Sanjay Kumar <94333583+sanjay-72@users.noreply.github.com>
2023-01-13 18:19:45 +0530

    Update Question4 answer.cpp

--- Question4 answer.cpp
+++ Question4 answer.cpp
@@ -1,1 +1,36 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+class IntI{
+private:
+	int i;
+public:
+	IntI(int i)
+	{
+		this->i=i;
+	}
+	void printNum()
+	{
+		cout<<"i = "<<i<<"\n";
+	}
+};
+class IntJ:public IntI
+{
+private:
+	int j;
+public:
+	IntJ(int i,int j):IntI(i)
+	{
+		this->j=j;
+	}
+	void printNum(){
+		IntI::printNum();
+		cout<<"j = "<<j<<"\n";
+	}
+};
+int main() {
+	IntJ intJ(3,3);
+	intJ.printNum();
+	system("pause");
+	return 0;
+}
